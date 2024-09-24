1da65438e0f13278ffc35abdd8bb8aa665017566
Carlos Padilla <cpadillaortega@gmail.com>
2024-09-24 00:46:30 -0400

    sign, abs and pow.

--- js/numbers.js
+++ js/numbers.js
@@ -7,6 +7,9 @@ console.log(Number.parseInt(15.9));
 //parseFloat
 console.log(Number.parseFloat(15.9));
 
+console.log("-");
+console.log("Math Methods");
+
 //Math Object
 //Round number
 console.log("Math round: ", Math.round(5.8));
@@ -17,3 +20,11 @@ console.log("Math floor: ", Math.floor(4.9));
 //Only keep the int number
 console.log("Math trunc: ", Math.trunc(17.7));
 // -1 = negative, 0 = 0 and 1 = positive.
+console.log("Math sign: ", Math.sign(15));
+//Only return a positive value
+console.log("Math abs", Math.abs(-15));
+
+//Power
+console.log("Math pow: ", Math.pow(2, 5));
+//or 
+console.log("Math pow or power: ", 2 ** 5);
